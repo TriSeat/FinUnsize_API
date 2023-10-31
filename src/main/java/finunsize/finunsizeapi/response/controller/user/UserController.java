36839d6736406611d3a3f42a72b950d40fe2d59f
Gui-Ribs <gr.estudios8@gmail.com>
2023-10-31 00:23:01 -0300

    chore/remove local user

--- src/main/java/finunsize/finunsizeapi/response/controller/user/UserController.java
+++ src/main/java/finunsize/finunsizeapi/response/controller/user/UserController.java
@@ -1,11 +1,11 @@
 package finunsize.finunsizeapi.response.controller.user;
 
 import finunsize.finunsizeapi.business.configuration.handler.user.ContextNullException;
-import finunsize.finunsizeapi.business.dto.user.plan.PlanUserLogin;
-import finunsize.finunsizeapi.business.dto.user.plan.PlanUserResponse;
-import finunsize.finunsizeapi.business.dto.user.plan.PlanUserSign;
-import finunsize.finunsizeapi.business.dto.user.plan.PlanUserUpdate;
-import finunsize.finunsizeapi.business.service.user.plan.UserService;
+import finunsize.finunsizeapi.business.dto.user.main.UserLogin;
+import finunsize.finunsizeapi.business.dto.user.main.UserResponse;
+import finunsize.finunsizeapi.business.dto.user.main.UserSign;
+import finunsize.finunsizeapi.business.dto.user.main.UserUpdate;
+import finunsize.finunsizeapi.business.service.user.main.UserService;
 import finunsize.finunsizeapi.integration.auth.UserSession;
 import finunsize.finunsizeapi.integration.auth.token.TokenService;
 import finunsize.finunsizeapi.persistence.model.user.UserModel;
@@ -15,60 +15,70 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("user")
-public class PlanUserController {
+public class UserController {
 
     private final AuthenticationManager authenticationManager;
-    private final UserService planUserService;
+    private final UserService userService;
     private final TokenService tokenService;
     private final UserSession userSession;
 
     @Autowired
-    public PlanUserController(AuthenticationManager authenticationManager, UserService planUserService, TokenService tokenService, UserSession userSession) {
+    public UserController(AuthenticationManager authenticationManager, UserService userService, TokenService tokenService, UserSession userSession) {
         this.authenticationManager = authenticationManager;
-        this.planUserService = planUserService;
+        this.userService = userService;
         this.tokenService = tokenService;
         this.userSession = userSession;
     }
 
     @PostMapping("login")
-    public ResponseEntity login(@RequestBody @Valid PlanUserLogin planUserDTO) {
-        var credentials = new UsernamePasswordAuthenticationToken(planUserDTO.nome(), planUserDTO.password());
+    public ResponseEntity login(@RequestBody @Valid UserLogin planUserDTO) {
+        var credentials = new UsernamePasswordAuthenticationToken(planUserDTO.login(), planUserDTO.password());
         var auth = this.authenticationManager.authenticate(credentials);
-        String token = tokenService.tokenGeneration((UserDetails) auth.getPrincipal());
+        String token = tokenService.tokenGeneration((UserModel) auth.getPrincipal());
 
         return ResponseEntity.ok().body(token);
     }
 
     @PostMapping("signup")
-    public ResponseEntity sign(@RequestBody @Valid PlanUserSign planUserSignDTO) {
-        planUserService.signUp(planUserSignDTO);
+    public ResponseEntity sign(@RequestBody @Valid UserSign planUserSignDTO) {
+        userService.signUp(planUserSignDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("User created");
     }
 
+    @GetMapping("list")
+    public ResponseEntity<List<UserResponse>> list() throws ContextNullException {
+        var users = userService.listUser();
+        return ResponseEntity.ok(users);
+    }
+
     @GetMapping("credentials")
-    public ResponseEntity<PlanUserResponse> list() throws ContextNullException {
-        String login = userSession.getCurrentLogin();
-        var user = planUserService.findUser(login);
+    public ResponseEntity<UserResponse> credentils() throws ContextNullException {
+        var user = userService.findLogin();
         return ResponseEntity.ok(user);
     }
 
+    @GetMapping("find/{username}")
+    public  ResponseEntity<UserResponse> findUser(@PathVariable String username) throws ContextNullException {
+        var user = userService.findUser(username);
+        return ResponseEntity.ok(user);
+    }
 
-    @PutMapping("update/{cnpj}")
-    public ResponseEntity updatePlanUser(@PathVariable String cnpj,@RequestBody @Valid PlanUserUpdate planUserUpdate) {
-        UserModel updatedUser = planUserService.updateUser(cnpj, planUserUpdate);
+    @PutMapping("update/{id}")
+    public ResponseEntity updatePlanUser(@PathVariable UUID id,@RequestBody @Valid UserUpdate planUserUpdate) {
+        userService.updateUser(id, planUserUpdate);
         return ResponseEntity.ok("Usuário Atualizado");
     }
 
     @DeleteMapping("delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable(value = "id") UUID id) {
-        planUserService.deleteUser(n);
-        return ResponseEntity.ok("Usuário da empresa " + cnpj + " excluído com sucesso");
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("Usuário da empresa " + id + " excluído com sucesso");
     }
 }
