74899bb74128cd0b8c75c02182d1f940c8054fe8
Gui-Ribs <gr.estudios8@gmail.com>
2023-10-31 00:23:48 -0300

    feat/search of CNPJ

--- src/main/java/finunsize/finunsizeapi/persistence/repository/user/CompanyRepository.java
+++ src/main/java/finunsize/finunsizeapi/persistence/repository/user/CompanyRepository.java
@@ -1,10 +1,8 @@
 package finunsize.finunsizeapi.persistence.repository.user;
 
-import finunsize.finunsizeapi.persistence.model.user.company.CompanyModel;
+import finunsize.finunsizeapi.persistence.model.user.CompanyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends JpaRepository <CompanyModel, String> {
--- src/main/java/finunsize/finunsizeapi/persistence/repository/user/UserRepository.java
+++ src/main/java/finunsize/finunsizeapi/persistence/repository/user/UserRepository.java
@@ -1,29 +1,32 @@
 package finunsize.finunsizeapi.persistence.repository.user;
 
-import finunsize.finunsizeapi.persistence.model.user.company.CompanyModel;
-import finunsize.finunsizeapi.persistence.model.user.plan.PlanUserModel;
+import finunsize.finunsizeapi.persistence.model.user.CompanyModel;
+import finunsize.finunsizeapi.persistence.model.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface PlanUserRepository extends JpaRepository <PlanUserModel, UUID> {
-    UserDetails findByNome(String name);
+public interface UserRepository extends JpaRepository <UserModel, UUID> {
+    List<UserModel> findAllByCnpj_Cnpj(String cnpj);
+    UserDetails findByLogin(String login);
+    Optional<UserModel> findByNomeAndCnpj_Cnpj(String login, String cnpj);
     boolean existsByNome(String name);
     boolean existsByEmail(String email);
 
     @Query(value = "SELECT * FROM plano_usuario p WHERE p.cnpj = :cnpj", nativeQuery = true)
-    Optional<PlanUserModel> findByCnpj(String cnpj);
+    Optional<UserModel> findByCnpj(String cnpj);
 
-    Optional<PlanUserModel> findById(UUID id);
-
+    Optional<UserModel> findById(UUID id);
 
     boolean existsByNomeAndCnpj_CnpjNot(String nome, String cnpj);;
 
     boolean existsByEmailAndCnpj_CnpjNot(String email, String cnpj);
+
+    boolean existsByLogin(String login);
 }
