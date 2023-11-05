db3e5e168255dcd197ad558212c437b02acb11b6
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:20:41 -0300

    fix/remove bugs

--- src/main/java/finunsize/finunsizeapi/business/configuration/handler/CustomExceptionHandler.java
+++ src/main/java/finunsize/finunsizeapi/business/configuration/handler/CustomExceptionHandler.java
@@ -1,7 +1,6 @@
 package finunsize.finunsizeapi.business.configuration.handler;
 
 import finunsize.finunsizeapi.business.configuration.handler.user.ContextNullException;
-import finunsize.finunsizeapi.business.configuration.handler.user.EntityAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
