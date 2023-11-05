0b8e2e713d317cbb7264a9d563290db2f632e9aa
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:22:02 -0300

    refactor/super template string changed

--- src/main/java/finunsize/finunsizeapi/business/configuration/handler/EntityAlreadyExistsException.java
+++ src/main/java/finunsize/finunsizeapi/business/configuration/handler/EntityAlreadyExistsException.java
@@ -1,7 +1,7 @@
-package finunsize.finunsizeapi.business.configuration.handler.user;
+package finunsize.finunsizeapi.business.configuration.handler;
 
 public class EntityAlreadyExistsException extends RuntimeException {
     public EntityAlreadyExistsException(String message, String... attributes) {
-        super(message + ": " + String.join(" & ", attributes));
+        super(message + "" + String.join(" & ", attributes));
     }
 }
