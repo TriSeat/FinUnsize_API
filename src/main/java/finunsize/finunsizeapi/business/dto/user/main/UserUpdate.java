742865bf4a80fe624e4ccff8f9eac16b39243876
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:26:42 -0300

    refactor/changing attributes

--- src/main/java/finunsize/finunsizeapi/business/dto/user/main/UserUpdate.java
+++ src/main/java/finunsize/finunsizeapi/business/dto/user/main/UserUpdate.java
@@ -1,5 +1,6 @@
 package finunsize.finunsizeapi.business.dto.user.main;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -7,7 +8,7 @@ public record UserUpdate(
         @NotBlank @Size(max = 50) String nome,
         @NotBlank @Size(max = 70) String login,
         @NotBlank String password,
-        @NotBlank @Size(max = 50) String email,
+        @Email @NotBlank @Size(max = 50) String email,
         int telefone,
         int cep,
         String url_image
