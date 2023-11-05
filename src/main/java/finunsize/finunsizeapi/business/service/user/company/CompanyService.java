e2b3f4c9dda5304a7c860c9b9ee6cbafbb7f08ad
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:28:28 -0300

    feat/add methods

--- src/main/java/finunsize/finunsizeapi/business/service/user/company/CompanyService.java
+++ src/main/java/finunsize/finunsizeapi/business/service/user/company/CompanyService.java
@@ -1,4 +1,14 @@
 package finunsize.finunsizeapi.business.service.user.company;
 
+import finunsize.finunsizeapi.business.configuration.handler.user.ContextNullException;
+import finunsize.finunsizeapi.business.dto.user.company.CompanyCreate;
+import finunsize.finunsizeapi.business.dto.user.company.CompanyResponse;
+import finunsize.finunsizeapi.business.dto.user.company.CompanyUpdate;
+import finunsize.finunsizeapi.persistence.model.user.CompanyModel;
+import jakarta.transaction.Transactional;
+
 public interface CompanyService {
+    CompanyModel create(CompanyCreate companyCreate);
+    CompanyResponse find() throws ContextNullException;
+    CompanyModel update(CompanyUpdate companyUpdate) throws ContextNullException;
 }
