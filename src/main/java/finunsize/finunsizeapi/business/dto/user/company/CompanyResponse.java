f329631c8672081ce6a791362f9f75ec300f7ab9
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:24:42 -0300

    refactor/changing attributes

--- src/main/java/finunsize/finunsizeapi/business/dto/user/company/CompanyResponse.java
+++ src/main/java/finunsize/finunsizeapi/business/dto/user/company/CompanyResponse.java
@@ -1,12 +1,25 @@
-package finunsize.finunsizeapi.business.dto.user;
+package finunsize.finunsizeapi.business.dto.user.company;
 
+import finunsize.finunsizeapi.persistence.model.user.CompanyModel;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.math.BigDecimal;
 
 public record CompanyResponse(
-        @NotBlank @Size(max = 14) long CNPJ,
+        @NotBlank @Size(max = 14) String cnpj,
         @NotBlank @Size(max = 100) String nome,
         @NotBlank @Size(max = 100) String slogan,
-        @Size(max = 8) int cep,
-        @NotBlank @Size(max = 100) String razao_social
-) {}
+        @NotBlank @Size(max = 60) String segmento,
+        int cep,
+        BigDecimal renda_media,
+        BigDecimal balanco_atual,
+        BigDecimal despesa_media,
+        @NotBlank @Size(max = 100) String razao,
+        String url_image
+) {
+    public CompanyResponse(CompanyModel company) {
+        this(company.getCnpj(), company.getNome(), company.getSlogan(), company.getSegmento(), company.getCep(), company.getRenda_media(), company.getBalanco_atual(), company.getDespesa_media(), company.getRazao(), company.getUrl_image());
+    }
+}
