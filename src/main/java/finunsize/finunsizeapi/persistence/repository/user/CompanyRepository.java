74b3a43f4263f059c9c661d398756fc603cc6e42
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:33:45 -0300

    feat/validation methods

--- src/main/java/finunsize/finunsizeapi/persistence/repository/user/CompanyRepository.java
+++ src/main/java/finunsize/finunsizeapi/persistence/repository/user/CompanyRepository.java
@@ -1,10 +1,20 @@
 package finunsize.finunsizeapi.persistence.repository.user;
 
+import finunsize.finunsizeapi.business.dto.user.company.CompanyResponse;
 import finunsize.finunsizeapi.persistence.model.user.CompanyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository <CompanyModel, String> {
-    CompanyModel findByCnpj(String cnpj);
+    CompanyModel findCnpjByCnpj(String cnpj);
+    Optional<CompanyModel> findByCnpj(String cnpj);
+    boolean existsByCnpj(String cnpj);
+    boolean existsByNomeAndSegmento(String name, String Segmento);
+    boolean existsByNomeAndSegmentoAndCnpjNot(String name, String Segmento, String cnpj);
+    boolean existsByRazao(String corporate_reason);
+    boolean existsByRazaoAndCnpjNot(String corporate_reason, String cnpj);
 }
