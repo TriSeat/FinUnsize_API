0fc97d3de94437a3cb1f7af455458852dafb546a
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:27:34 -0300

    feat/use case of company created

--- src/main/java/finunsize/finunsizeapi/business/service/user/company/Company.java
+++ src/main/java/finunsize/finunsizeapi/business/service/user/company/Company.java
@@ -1,4 +1,62 @@
 package finunsize.finunsizeapi.business.service.user.company;
 
+import finunsize.finunsizeapi.business.configuration.handler.user.ContextNullException;
+import finunsize.finunsizeapi.business.dto.user.company.CompanyCreate;
+import finunsize.finunsizeapi.business.dto.user.company.CompanyResponse;
+import finunsize.finunsizeapi.business.dto.user.company.CompanyUpdate;
+import finunsize.finunsizeapi.integration.auth.UserSession;
+import finunsize.finunsizeapi.persistence.model.user.CompanyModel;
+import finunsize.finunsizeapi.persistence.repository.user.CompanyRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Company implements CompanyService {
+    private final CompanyRepository companyRepository;
+    private final UserSession userSession;
+    private final ValidationCompany validationCompany;
+
+    @Autowired
+    public Company(CompanyRepository companyRepository, UserSession userSession, ValidationCompany validationCompany) {
+        this.companyRepository = companyRepository;
+        this.userSession = userSession;
+        this.validationCompany = validationCompany;
+    }
+
+    @Transactional
+    @Override
+    public CompanyModel create(@Valid CompanyCreate companyCreate) {
+        CompanyModel companyModel = new CompanyModel();
+        validationCompany.valid(companyCreate.cnpj(), companyCreate.nome(), companyCreate.segmento(), companyCreate.razao());
+        BeanUtils.copyProperties(companyCreate, companyModel);
+        return companyRepository.save(companyModel);
+    }
+
+    @Override
+    public CompanyResponse find() throws ContextNullException {
+        var cnpj = getCnpj();
+        var company = companyRepository.findByCnpj(cnpj)
+                .orElseThrow(() -> new RuntimeException("Empresa não encontrada ou excluída"));
+        return new CompanyResponse(company);
+    }
+
+    @Transactional
+    @Override
+    public CompanyModel update(@Valid CompanyUpdate companyUpdate) throws ContextNullException {
+        var cnpj = getCnpj();
+        var company = companyRepository.findByCnpj(cnpj)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Empresa com o cnpj de %s não contém um cadastro", cnpj)));
+        validationCompany.validUpdate(companyUpdate.nome(), companyUpdate.segmento(), cnpj, companyUpdate.razao_social());
+        BeanUtils.copyProperties(companyUpdate, company, "cnpj");
+        return companyRepository.save(company);
+    }
+
+    private String getCnpj() throws ContextNullException {
+        String cnpj = userSession.getSessionCnpj();
+        return cnpj;
+    }
 }
