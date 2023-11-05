b46762227f11f3144a6f3e051b65efb2b6c5e8c4
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-05 20:18:16 -0300

    chore/remove cnpj dependency

--- src/main/java/finunsize/finunsizeapi/business/configuration/handler/EntityAlreadyExistsException.java
+++ src/main/java/finunsize/finunsizeapi/business/configuration/handler/EntityAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package finunsize.finunsizeapi.business.configuration.handler.user;
 
 public class EntityAlreadyExistsException extends RuntimeException {
-    public EntityAlreadyExistsException(String name, String email) {
-        super("Nome ou email já existem: " + name + " & " + email);
+    public EntityAlreadyExistsException(String message, String... attributes) {
+        super(message + ": " + String.join(" & ", attributes));
     }
 }
