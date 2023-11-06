feec28e4abe99c83688a32ec43f20c0f8350f2dc
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-06 10:47:40 -0300

    feat/ add address

--- src/main/java/finunsize/finunsizeapi/business/service/address/Address.java
+++ src/main/java/finunsize/finunsizeapi/business/service/address/Address.java
@@ -1,7 +1,15 @@
 package finunsize.finunsizeapi.business.service.address;
 
+import finunsize.finunsizeapi.business.dto.address.AddressCreate;
+import finunsize.finunsizeapi.business.dto.address.AddressUpdate;
+import finunsize.finunsizeapi.persistence.model.address.AddressModel;
 import finunsize.finunsizeapi.persistence.repository.address.AddressRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class Address implements AddressService {
@@ -12,4 +20,21 @@ public class Address implements AddressService {
     }
 
 
+    @Override
+    public AddressModel create(@Valid AddressCreate addressCreate) {
+        AddressModel addressModel = new AddressModel();
+        BeanUtils.copyProperties(addressCreate, addressModel, "id_logradouro");
+        return addressRepository.save(addressModel);
+    }
+
+    @Override
+    public AddressModel update(UUID id, @Valid AddressUpdate addressUpdate) {
+        var address = addressRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Endereço não foi encontrado: %s", id)));
+        BeanUtils.copyProperties(addressUpdate, address, "id_logradouro");
+        return addressRepository.save(address);
+    }
+
+    @Override
+    public void delete() {}
 }
--- src/main/java/finunsize/finunsizeapi/business/service/address/AddressService.java
+++ src/main/java/finunsize/finunsizeapi/business/service/address/AddressService.java
@@ -1,4 +1,15 @@
 package finunsize.finunsizeapi.business.service.address;
 
+import finunsize.finunsizeapi.business.dto.address.AddressCreate;
+import finunsize.finunsizeapi.business.dto.address.AddressUpdate;
+import finunsize.finunsizeapi.persistence.model.address.AddressModel;
+
+import java.util.UUID;
+
 public interface AddressService {
+
+    AddressModel create(AddressCreate addressCreate);
+    AddressModel update(UUID id, AddressUpdate addressUpdate);
+    void delete();
+
 }
