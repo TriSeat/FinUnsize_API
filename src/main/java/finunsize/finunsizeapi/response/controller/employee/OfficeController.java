63128ed4a0ae2d2af9c5250b0de28d014a897179
Gui-Ribs <gr.estudios8@gmail.com>
2023-11-14 23:39:48 -0300

    feat: add employee controller

---
+++ src/main/java/finunsize/finunsizeapi/response/controller/employee/OfficeController.java
@@ -0,0 +1,52 @@
+package finunsize.finunsizeapi.response.controller.employee;
+
+import finunsize.finunsizeapi.business.configuration.handler.user.ContextNullException;
+import finunsize.finunsizeapi.business.dto.employee.office.OfficeCreate;
+import finunsize.finunsizeapi.business.dto.employee.office.OfficeResponse;
+import finunsize.finunsizeapi.business.service.employee.OfficeService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("office")
+public class OfficeController {
+
+    private final OfficeService officeService;
+
+    public OfficeController(OfficeService officeService) {
+        this.officeService = officeService;
+    }
+
+    @PostMapping("create")
+    public ResponseEntity create(@RequestBody @Valid OfficeCreate officeCreate) throws ContextNullException {
+        officeService.create(officeCreate);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Cargo criado com sucesso");
+    }
+
+    @GetMapping("find/{name}")
+    public ResponseEntity<OfficeResponse> find(@PathVariable String name) throws ContextNullException {
+        return ResponseEntity.ok(officeService.find(name));
+    }
+
+    @GetMapping("list")
+    public ResponseEntity<List<OfficeResponse>> list() throws ContextNullException {
+        return ResponseEntity.ok(officeService.list());
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity update(@PathVariable UUID id, @RequestBody @Valid OfficeCreate officeCreate) {
+        officeService.update(id, officeCreate);
+        return ResponseEntity.ok("Funcionário atualizado com sucesso");
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity delete(@PathVariable UUID id) {
+        officeService.delete(id);
+        return ResponseEntity.ok("Cargo excluído com sucesso");
+    }
+}
